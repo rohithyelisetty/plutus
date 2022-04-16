0ba8dcf88c540f6bd11ed1db5d699bc42227e233
rohithyelisetty <75509716+rohithyelisetty@users.noreply.github.com>
2022-04-16 11:59:29 -0400

    Clean up

---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp0U1pdcwjYUI7q-v8So9zGqVR7spC_2U",
+  authDomain: "plutus-c0e90.firebaseapp.com",
+  projectId: "plutus-c0e90",
+  storageBucket: "plutus-c0e90.appspot.com",
+  messagingSenderId: "921078766196",
+  appId: "1:921078766196:web:0a53afbdd1e48d680b4f21"
+};
+
+const app = initializeApp(firebaseConfig);
